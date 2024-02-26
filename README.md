2d87d85c72bdba6612e616f0c0eb92cf68a8f469
1bsilver <ibrahim.silver20@gmail.com>
2024-02-27 03:55:47 +0530

    Migrated to vue3

---
+++ README.md
@@ -0,0 +1,106 @@
+# vuetify3-audio-player
+
+<a href="https://www.npmjs.com/package/vuetify-audio"><img src="https://img.shields.io/npm/dt/vuetify-audio.svg" alt="Downloads"></a>
+<a href="https://www.npmjs.com/package/vuetify-audio"><img src="https://img.shields.io/npm/v/vuetify-audio.svg" alt="Version"></a>
+<a href="https://www.npmjs.com/package/vuetify-audio"><img src="https://img.shields.io/npm/l/vuetify-audio.svg" alt="License"></a>
+
+Vue 3 sound audio player based on Vuetify UI framework. Covers audio-tag API and adds more.
+
+## Features
+
+- Supports Vue 3 and Vuetify 3.
+- Support most of audio play in this component.
+- You can set the color you want for all component buttons.
+- Support download the audio file.
+- After audio playing finished or before start the playing, you can do something.
+- Support Dark mode of Vuetify.
+- Support auto play, but if user didn't interact with the document first, the audio can't be played.
+- Support turn on and off audio download button.
+- Support disable the Vuetify Card style, and you can use this component in your own Vuetify Card of your page.
+- You can set custom icons supported by vuetify v-icon component.
+
+### Demo
+
+https://wilsonwu.github.io/dist/index.html#/vuetifyaudio
+
+### Installation
+
+Use npm: `npm install vuetify-audio --save`
+
+### Prepare
+
+At first make sure your project is Vue project, and has `Vuetify` as UI framework:
+
+1. Install Vuetify:
+
+```
+npm install vuetify --save-dev
+```
+
+2. Add Vuetify to `app.js` or `main.js`:
+
+```js
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
+
+Vue.use(Vuetify);
+```
+
+You also can use Vue plugin to install `Vuetify` by only one line command:
+
+```
+vue add vuetify
+```
+
+Node: Make sure you are using the default Vuetify iconfont (mdi) or override the icon attributes with some other supported by v-icon component.
+
+### Usage
+
+Add below code into your `<script>`:
+
+```js
+export default {
+  components: {
+    VuetifyAudio: () => import("vuetify-audio"),
+  },
+  data: () => ({
+    file: "http://www.hochmuth.com/mp3/Boccherini_Concerto_478-1.mp3",
+  }),
+};
+```
+
+And below code in the `<template>`:
+
+```html
+<vuetify-audio
+  :file="file"
+  color="success"
+  :ended="audioFinish"
+  downloadable
+></vuetify-audio>
+```
+
+### Attributes
+
+- **file** (String) (Required): Set audio file for the audio player
+- **ended** (Function) (Optional): Set callback function name after audio finish
+- **canPlay** (Function) (Optional): Set callback function name when audio ready for playing
+- **color** (String) (Optional): Set all component buttons color
+- **autoPlay** (Boolean) (Optional, default is false): Add it to make the audio auto play, but in some web browsers maybe failed, because some browsers need user active in the page first then allow sound auto play.
+- **downloadable** (Boolean) (Optional, default is false): Add it to let the audio file can be downloaded.
+- **flat** (Boolean) (Optional, default is false): When set to true, make the Vuetify Card style to flat, that you can combine other information/image/data with this control in your page.
+- **playIcon** (String) (Optional, default is mdi-play): Set the icon for play
+- **pauseIcon** (String) (Optional, default is mdi-pause): Set the icon for pause
+- **stopIcon** (String) (Optional, default is mdi-stop): Set the icon for stop
+- **refreshIcon** (String) (Optional, default is mdi-refresh): Set the icon for refresh
+- **downloadIcon** (String) (Optional, default is mdi-download): Set the icon for download
+- **volumeHighIcon** (String) (Optional, default is mdi-volume-high): Set the icon for volume
+- **volumeMuteIcon** (String) (Optional, default is mdi-volume-mute): Set the icon for mute
+
+### Known Issues
+
+1. Audio play pregress bar can't support drag, only support click.
+
+### License
+
+MIT
---
+++ src/vuetifyaudio.vue
@@ -0,0 +1,313 @@
+<template>
+  <v-container fluid>
+    <v-card
+      style="text-align: center"
+      :flat="flat == undefined || flat == false ? false : true"
+    >
+      <v-card-text>
+        <v-row>
+          <v-col class="px-0 mx-0">{{ currentTime }}</v-col>
+          <v-col cols="8">
+            <v-progress-linear
+              :color="color"
+              hide-details
+              v-model="percentage"
+              :height="20"
+              clickable
+              rounded
+              @click="setPosition()"
+              :disabled="!loaded"
+            ></v-progress-linear>
+          </v-col>
+          <v-col class="px-0 mx-0">{{ duration }}</v-col>
+        </v-row>
+      </v-card-text>
+      <audio
+        id="player"
+        ref="player"
+        v-on:ended="ended"
+        v-on:canplay="canPlay"
+        :src="file"
+      ></audio>
+      <v-card-actions>
+        <v-btn
+          variant="outlined"
+          icon
+          class="ma-2"
+          :color="color"
+          @click="playing ? pause() : play()"
+          :disabled="!loaded"
+        >
+          <v-icon v-if="!playing || paused">{{ playIcon }}</v-icon>
+          <v-icon v-else>{{ pauseIcon }}</v-icon>
+        </v-btn>
+        <v-btn
+          variant="outlined"
+          icon
+          class="ma-2"
+          :color="color"
+          @click="stop()"
+          :disabled="!loaded"
+        >
+          <v-icon>{{ stopIcon }}</v-icon>
+        </v-btn>
+        <v-btn
+          variant="outlined"
+          icon
+          class="ma-2"
+          :color="color"
+          @click="switchIsOnRepeat()"
+          :disabled="!loaded"
+        >
+          <v-icon v-if="isOnRepeat">mdi-repeat</v-icon>
+          <v-icon v-else>mdi-repeat-off</v-icon>
+        </v-btn>
+        <v-btn
+          variant="outlined"
+          icon
+          class="ma-2"
+          :color="color"
+          @click="loaded ? download() : reload()"
+          v-if="!loaded"
+        >
+          <v-icon>{{ refreshIcon }}</v-icon>
+        </v-btn>
+        <v-btn
+          variant="outlined"
+          icon
+          class="ma-2"
+          :color="color"
+          @click="loaded ? download() : reload()"
+          v-if="loaded && downloadable"
+        >
+          <v-icon>{{ downloadIcon }}</v-icon>
+        </v-btn>
+
+        <v-spacer></v-spacer>
+        <v-slider
+          v-model="playerVolume"
+          rounded
+          thumb-size="15"
+          hide-details
+          :color="color"
+          max="1"
+          step="0.01"
+          min="0"
+          density="compact"
+          class=""
+        >
+          <template #prepend>
+            <v-icon v-if="!isMuted" @click="mute()" :color="color">
+              {{ volumeHighIcon }}
+            </v-icon>
+            <v-icon v-else @click="mute()" :color="color">
+              {{ volumeMuteIcon }}
+            </v-icon>
+          </template>
+        </v-slider>
+      </v-card-actions>
+    </v-card>
+  </v-container>
+</template>
+<script>
+const formatTime = (second) =>
+  new Date(second * 1000).toISOString().substr(11, 8);
+
+export default {
+  name: "Vuetify3Player",
+  props: {
+    flat: {
+      type: Boolean,
+      default: false,
+    },
+    file: {
+      type: String,
+      default: null,
+    },
+    autoPlay: {
+      type: Boolean,
+      default: false,
+    },
+    ended: {
+      type: Function,
+      default: () => {},
+    },
+    canPlay: {
+      type: Function,
+      default: () => {},
+    },
+    color: {
+      type: String,
+      default: null,
+    },
+    downloadable: {
+      type: Boolean,
+      default: false,
+    },
+    playIcon: {
+      type: String,
+      default: "mdi-play",
+    },
+    pauseIcon: {
+      type: String,
+      default: "mdi-pause",
+    },
+    stopIcon: {
+      type: String,
+      default: "mdi-stop",
+    },
+    refreshIcon: {
+      type: String,
+      default: "mdi-refresh",
+    },
+    downloadIcon: {
+      type: String,
+      default: "mdi-download",
+    },
+    volumeHighIcon: {
+      type: String,
+      default: "mdi-volume-high",
+    },
+    volumeMuteIcon: {
+      type: String,
+      default: "mdi-volume-mute",
+    },
+  },
+  computed: {
+    duration: function () {
+      return this.audio ? formatTime(this.totalDuration) : "";
+    },
+  },
+  watch: {
+    playerVolume(v) {
+      if (v > 0) {
+        this.isMuted = false;
+      } else {
+        this.isMuted = true;
+      }
+    },
+  },
+  data() {
+    return {
+      firstPlay: true,
+      isMuted: false,
+      isOnRepeat: false,
+      loaded: false,
+      playing: false,
+      paused: false,
+      percentage: 0,
+      currentTime: "00:00:00",
+      audio: undefined,
+      totalDuration: 0,
+      playerVolume: 0.5,
+    };
+  },
+
+  methods: {
+    setPosition() {
+      this.audio.currentTime = parseInt(
+        (this.audio.duration / 100) * this.percentage
+      );
+    },
+    stop() {
+      this.audio.pause();
+      this.paused = true;
+      this.playing = false;
+      this.audio.currentTime = 0;
+    },
+    switchIsOnRepeat() {
+      this.isOnRepeat = !this.isOnRepeat;
+    },
+    play() {
+      if (this.playing) return;
+      this.audio.play().then(() => (this.playing = true));
+      this.paused = false;
+    },
+    pause() {
+      this.paused = !this.paused;
+      this.paused ? this.audio.pause() : this.audio.play();
+    },
+    download() {
+      this.audio.pause();
+      window.open(this.file, "download");
+    },
+    mute() {
+      this.isMuted = !this.isMuted;
+      this.playerVolume = this.isMuted ? 0 : 0.5;
+      this.audio.muted = this.isMuted;
+    },
+    reload() {
+      this.audio.load();
+    },
+    _handleLoaded: function () {
+      if (this.audio.readyState >= 2) {
+        if (this.audio.duration === Infinity) {
+          // Fix duration for streamed audio source or blob based
+          // https://stackoverflow.com/questions/38443084/how-can-i-add-predefined-length-to-audio-recorded-from-mediarecorder-in-chrome/39971175#39971175
+          this.audio.currentTime = 1e101;
+          this.audio.ontimeupdate = () => {
+            this.audio.ontimeupdate = () => {};
+            this.audio.currentTime = 0;
+            this.totalDuration = parseInt(this.audio.duration);
+            this.loaded = true;
+          };
+        } else {
+          this.totalDuration = parseInt(this.audio.duration);
+          this.loaded = true;
+        }
+
+        if (this.autoPlay) this.audio.play();
+      } else {
+        throw new Error("Failed to load sound file");
+      }
+    },
+    _handlePlayingUI: function () {
+      this.audio.volume = this.playerVolume;
+      this.percentage = (this.audio.currentTime / this.audio.duration) * 100;
+      this.currentTime = formatTime(this.audio.currentTime);
+      this.playing = true;
+    },
+    _handlePlayPause: function (e) {
+      if (e.type === "play" && this.firstPlay) {
+        // in some situations, audio.currentTime is the end one on chrome
+        this.audio.currentTime = 0;
+        if (this.firstPlay) {
+          this.firstPlay = false;
+        }
+      }
+      if (
+        e.type === "pause" &&
+        this.paused === false &&
+        this.playing === false
+      ) {
+        this.currentTime = "00:00:00";
+      }
+    },
+    _handleEnded() {
+      if (this.isOnRepeat) {
+        this.audio.play();
+      } else {
+        this.paused = this.playing = false;
+      }
+    },
+    init: function () {
+      this.audio.addEventListener("timeupdate", this._handlePlayingUI);
+      this.audio.addEventListener("loadeddata", this._handleLoaded);
+      this.audio.addEventListener("pause", this._handlePlayPause);
+      this.audio.addEventListener("play", this._handlePlayPause);
+      this.audio.addEventListener("ended", this._handleEnded);
+    },
+  },
+  mounted() {
+    this.audio = this.$refs.player;
+    this.init();
+  },
+  beforeUnmount() {
+    this.audio.removeEventListener("timeupdate", this._handlePlayingUI);
+    this.audio.removeEventListener("loadeddata", this._handleLoaded);
+    this.audio.removeEventListener("pause", this._handlePlayPause);
+    this.audio.removeEventListener("play", this._handlePlayPause);
+    this.audio.removeEventListener("ended", this._handleEnded);
+  },
+};
+</script>
